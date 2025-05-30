0a397bee43fbf437f71d96339dc61e4bf9d176d4
Ogawa <3223mq118@gmail.com>
2025-05-31 03:26:42 +0900

    readme

--- main.c
+++ main.c
@@ -9,3 +9,16 @@
  *  Note         : コマンド番号を受け取り，これを引数としてhandole関数に渡す．
  *  History      : 2025/05/24 - ファイル作成開始
  *****************************************************************/
+
+ #include <stdio.h>
+ #include "database.h"
+
+ int main(void) {
+    int command = 0;
+    printf("コマンドを入力してください：");
+    scanf("%d",&command);
+    hundole_command(command);
+
+    return 0;
+
+ }
